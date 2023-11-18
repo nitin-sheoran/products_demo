355f19384b44054479c49c003975c178c7c48648
Nitin <nsheoran2005@gmail.com>
2023-11-18 13:31:32 +0530

    model classes created

---
+++ lib/products/model/product_model.dart
@@ -0,0 +1,56 @@
+class Products {
+  int? id;
+  String? title;
+  String? description;
+  num? price;
+  num? discountPercentage;
+  num? rating;
+  num? stock;
+  String? brand;
+  String? category;
+  String? thumbnail;
+  List<String>? images;
+
+  Products(
+      {this.id,
+        this.title,
+        this.description,
+        this.price,
+        this.discountPercentage,
+        this.rating,
+        this.stock,
+        this.brand,
+        this.category,
+        this.thumbnail,
+        this.images});
+
+  Products.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    description = json['description'];
+    price = json['price'];
+    discountPercentage = json['discountPercentage'];
+    rating = json['rating'];
+    stock = json['stock'];
+    brand = json['brand'];
+    category = json['category'];
+    thumbnail = json['thumbnail'];
+    images = json['images'].cast<String>();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = id;
+    data['title'] = title;
+    data['description'] = this.description;
+    data['price'] = this.price;
+    data['discountPercentage'] = this.discountPercentage;
+    data['rating'] = this.rating;
+    data['stock'] = this.stock;
+    data['brand'] = this.brand;
+    data['category'] = this.category;
+    data['thumbnail'] = this.thumbnail;
+    data['images'] = this.images;
+    return data;
+  }
+}
---
+++ lib/products/model/products_model.dart
@@ -0,0 +1,31 @@
+class Product {
+  List<Product>? products;
+  int? total;
+  int? skip;
+  int? limit;
+
+  Product({this.products, this.total, this.skip, this.limit});
+
+  Product.fromJson(Map<String, dynamic> json) {
+    if (json['products'] != null) {
+      products = <Product>[];
+      json['products'].forEach((v) {
+        products!.add(Product.fromJson(v));
+      });
+    }
+    total = json['total'];
+    skip = json['skip'];
+    limit = json['limit'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (products != null) {
+      data['products'] = products!.map((v) => v.toJson()).toList();
+    }
+    data['total'] = total;
+    data['skip'] = skip;
+    data['limit'] = limit;
+    return data;
+  }
+}
